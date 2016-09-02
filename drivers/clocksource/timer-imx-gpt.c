3d69fc2f60adae1f297e5ac4a14b13244b40eb80
Troy Kisky <troy.kisky@boundarydevices.com>
2016-09-01 18:25:55 -0700

    time: imx6q tapeout 1.0 cannot use osc_per
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/clocksource/timer-imx-gpt.c
+++ drivers/clocksource/timer-imx-gpt.c
@@ -499,7 +499,9 @@ static int __init mxc_timer_init_dt(struct device_node *np,  enum imx_gpt_type t
 	imxtm->clk_ipg = of_clk_get_by_name(np, "ipg");
 
 	/* Try osc_per first, and fall back to per otherwise */
-	imxtm->clk_per = of_clk_get_by_name(np, "osc_per");
+	imxtm->clk_per = (cpu_is_imx6q() &&
+		imx_get_soc_revision() <= IMX_CHIP_REVISION_1_0) ?
+		ERR_PTR(-ENODEV) : of_clk_get_by_name(np, "osc_per");
 	if (IS_ERR(imxtm->clk_per))
 		imxtm->clk_per = of_clk_get_by_name(np, "per");
 
